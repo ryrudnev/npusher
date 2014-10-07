2db3532a2599cdddc871cfecbfeb97c5230a52ed
rudnev <ryrudnev@gmail.com>
2014-10-07 10:49:07 +0400

    Added timeout for curl connection and remember dns cache. Also added errors handling.

--- API libraries/PHP/lib/Pusher.php
+++ API libraries/PHP/lib/Pusher.php
@@ -27,8 +27,9 @@ class Pusher {
      * @param string     $secret key for authentication
      * @param string     $host host of pusher
      * @param int        $port port of pusher
+     * @param int        $timeout waiting time for connection
      */
-    public function __construct($app, $token, $secret, $host, $port) {
+    public function __construct($app, $token, $secret, $host, $port, $timeout=5) {
         if (!extension_loaded('curl') || !extension_loaded('json')) {
             throw new PusherException('There is missing dependant extensions - need cUrl and JSON');
         }
@@ -38,6 +39,7 @@ class Pusher {
         $this->_config['port'] = $port;
         $this->_config['token'] = $token;
         $this->_config['secret'] = $secret;
+        $this->_config['timeout'] = $timeout;
 
         $this->_config['url'] = $this->_config['server'] . ':' . $this->_config['port'] . '/api/apps/' . $this->_config['app'];
     }
@@ -54,25 +56,34 @@ class Pusher {
         
         $full_url = $this->_config['url'] . $path;
         
-        $ch = curl_init($full_url);
+        $ch = curl_init();
+        if ($ch === false) {
+            throw new PusherException('Could not initialise cURL');
+	}
         
+        curl_setopt($ch, CURLOPT_URL, $full_url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             'Authorization: Basic ' . base64_encode($this->_config['token'] . ':' . $this->_config['secret']),
             'Content-Type: application/json',
             'Content-Length: ' . strlen($json))
         );
         
-        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $this->_config['timeout']);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->_config['timeout'] / 2);
+        curl_setopt($ch, CURLOPT_DNS_CACHE_TIMEOUT, $this->_config['timeout'] * 2);
 
         $response = array();
         
         $response['body'] = curl_exec($ch);
         $response['status'] = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
+        
+        if (curl_errno($ch) !== 0) {
+            throw new PusherException('cUrl error: ' . curl_error($ch));    
+        }
+        
         curl_close($ch);
         
         return $response;
